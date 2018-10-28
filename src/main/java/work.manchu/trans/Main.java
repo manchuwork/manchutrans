4217b41333dd77d19388f2da4805fa0223ed75f3
heshen <manchuwork@163.com>
2018-10-28 10:51:36 +0800

    first commit

---
+++ src/main/java/work.manchu/trans/Main.java
@@ -0,0 +1,16 @@
+package work.manchu.trans;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        TransNodeProcesser transNodeProcesser = new TransNodeProcesser();
+        String ret = transNodeProcesser.trans("Sxd");
+        log.info("ret2:{}",ret);
+    }
+}
